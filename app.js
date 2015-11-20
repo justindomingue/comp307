1fe65c6320ea83b1636e8afb4302bb89eaf102f9
keith471 <keithstrickling@gmail.com>
2015-11-20 13:19:09 -0500

    Partially-implemented group chat functionality

--- app.js
+++ app.js
@@ -14,66 +14,147 @@ app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 
-// usernames which are currently connected to the chat
+// Initialize our dictionary of rooms with the global room "Free-for-all"
+// Key = name of room
+// Value = object containing usernames and number of participants
+var rooms = {}
+rooms["Free-for-all"] = {
+	numUsers: 0,
+	usernames: {}
+}
+
+// Dictionary of users currently online
+// Key: username
+// Value: a dictionary of rooms the user is in
+// As an example, here is what usernames might look like:
+/*
+var usernames = {
+    keith: {
+        room1: "room1",
+        room2: "room2",
+        room3: "room3"
+    },
+    jane: {
+        room1: "room1"
+    }
+}
+*/
 var usernames = {};
 var numUsers = 0;
 
 io.on('connection', function (socket) {
+  // Field to indicate whether a user corresponding to the given socket has been added
+  // This field will be set to true if the user logs in
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
+    socket.to(data.room).emit('new message', {
       username: socket.username,
-      message: data
+      room: data.room,
+      message: data.message
     });
   });
-
+	
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
+  // handles new users added to the system by storing their username in the socket session,
+  // and emitting a welcome message to them
+  socket.on('add user', function (data) {
     // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    ++numUsers;
+    socket.username = data.username;
+    // add the client's username to the list of all active users
+    usernames[data.username] = {};
+    // increment the number of users in the room by one
+    numUsers++;
     addedUser = true;
+    // Emit a message back to the user to indicate that they are logged into the chat
+    // and to welcome them to the chat
     socket.emit('login', {
       numUsers: numUsers
     });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
+  });
+  
+  socket.on('add room', function (data) {
+    // TODO: implement
+  });
+  
+  socket.on('user joined', function (data) {
+    // subscribe the socket to the room
+    socket.join(data.room);
+    // Add the room to the list of rooms the user is a part of
+    usernames[data.username][data.room] = data.room;
+    // add the client's username to the list of usernames for the current chat
+    rooms[data.room].usernames[data.username] = data.username;
+    // increment the number of users in the room by one
+    rooms[data.room].numUsers++;
+    // echo to all members of the chat that a person has connected
+    socket.to(data.room).emit('user joined', {
+      room: data.room,
       username: socket.username,
-      numUsers: numUsers
+      numUsers: rooms[data.room].numUsers
     });
   });
 
   // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
+  socket.on('typing', function (data) {
+    socket.to(data.room).emit('typing', {
+      username: socket.username,
+      room: data.room
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
+  socket.on('stop typing', function (data) {
+    socket.to(data.room).emit('stop typing', {
+      username: socket.username,
+      room: data.room
     });
   });
 
-  // when the user disconnects.. perform this
+  // when the user closes the webpage, disconnect them from all groups they were in
   socket.on('disconnect', function () {
-    // remove the username from global usernames list
+    // remove the username from all the rooms the user
+    // was a part of and from the list of global usernames
     if (addedUser) {
+      // Get the dictionary of rooms the user was a part of
+      var usersRooms = usernames[socket.username];
+      // Iterate through the rooms, removing the user from each one, and echoing to other
+      // members of each room that the user has left
+      for (var room in usersRooms) {
+        removeUserFromRoom(usersRooms[room]);
+      }
+      // Remove the user from the global list of usernames
       delete usernames[socket.username];
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
+      numUsers--;
     }
   });
+  
+  // when the user closes an individual tab, disconnect them from that group
+  socket.on('disconnect from group', function(data) {
+    if (addedUser) {
+      removeUserFromRoom(data.room);
+      delete usernames[socket.username][data.room];
+    }
+  });
+  
+  // Helper function which removes the user from the given room and notifies other participants
+  // of the room
+  function removeUserFromRoom(room) {
+    delete rooms[room].usernames[socket.username];
+    rooms[room].numUsers--;
+	if (rooms[room].numUsers === 0) {
+	  // Remove the room entirely
+	  delete rooms[room];
+	} else {
+	  // Emit to the remaining users that the user has left
+	  socket.to(room).emit('user left', {
+		username: socket.username,
+		room: room,
+		numUsers: rooms[room].numUsers
+	  });
+	}
+	// Have the socket leave the room
+    socket.leave(room);
+  }
 });
--- public/main.js
+++ public/main.js
@@ -6,11 +6,14 @@ $(function() {
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
   ];
+  
+  // My variables
+  var defaultRoom = "Free-for-all"; // The global room is the default that the user logs into
+  var activeRoom = defaultRoom;	// The active room is the default to start
 
   // Initialize varibles
   var $window = $(window);
   var $usernameInput = $('.usernameInput'); // Input for username
-  var $messages = $('.messages'); // Messages area
   var $inputMessage = $('.inputMessage'); // Input message input box
 
   var $loginPage = $('.login.page'); // The login page
@@ -25,14 +28,26 @@ $(function() {
 
   var socket = io();
 
+  //  Sets a message to display upon adding a participant to a chat
   function addParticipantsMessage (data) {
     var message = '';
     if (data.numUsers === 1) {
-      message += "there's 1 participant";
+      message += "this room has 1 participant";
     } else {
-      message += "there are " + data.numUsers + " participants";
+      message += "this room has " + data.numUsers + " participants";
     }
-    log(message);
+    log(message, data.room);
+  }
+  
+  // Sets a message to display upon logging in a new user
+  function newUserMessage (data) {
+    var message = '';
+    if (data.numUsers === 1) {
+      message += "there is 1 user currently online";
+    } else {
+      message += "there are " + data.numUsers + " users currently online";
+    }
+    log(message, defaultRoom);
   }
 
   // Sets the client's username
@@ -46,8 +61,9 @@ $(function() {
       $loginPage.off('click');
       $currentInput = $inputMessage.focus();
 
-      // Tell the server your username
-      socket.emit('add user', username);
+      // Tell the server your username and the room you've joined
+      socket.emit('add user', {username: username});
+      socket.emit('user joined', {username: username, room: defaultRoom});
     }
   }
 
@@ -61,17 +77,20 @@ $(function() {
       $inputMessage.val('');
       addChatMessage({
         username: username,
+        room: activeRoom,
         message: message
       });
       // tell server to execute 'new message' and send along one parameter
-      socket.emit('new message', message);
+      // TODO: remove next line
+      //$(".messages").append('<li>' + message + '</li>');
+      socket.emit('new message', {message: message, room: activeRoom});
     }
   }
 
   // Log a message
-  function log (message, options) {
+  function log (message, room, options) {
     var $el = $('<li>').addClass('log').text(message);
-    addMessageElement($el, options);
+    addMessageElement($el, room, options);
   }
 
   // Adds the visual chat message to the message list
@@ -96,7 +115,7 @@ $(function() {
       .addClass(typingClass)
       .append($usernameDiv, $messageBodyDiv);
 
-    addMessageElement($messageDiv, options);
+    addMessageElement($messageDiv, data.room, options);
   }
 
   // Adds the visual chat typing message
@@ -115,12 +134,17 @@ $(function() {
 
   // Adds a message element to the messages and scrolls to the bottom
   // el - The element to add as a message
+  // room - The chat room to add it to
   // options.fade - If the element should fade-in (default = true)
   // options.prepend - If the element should prepend
   //   all other messages (default = false)
-  function addMessageElement (el, options) {
+  function addMessageElement (el, room, options) {
     var $el = $(el);
-
+    
+    
+    // Create a jQuery selector for the room in which to add the message
+    var $messages = $('#' + room + ' > div > ul');
+    
     // Setup default options
     if (!options) {
       options = {};
@@ -141,6 +165,8 @@ $(function() {
     } else {
       $messages.append($el);
     }
+    // Scroll to the bottom of the messages
+    // TODO: consider changing this. Might want to add the ability to manually scroll the page
     $messages[0].scrollTop = $messages[0].scrollHeight;
   }
 
@@ -154,7 +180,7 @@ $(function() {
     if (connected) {
       if (!typing) {
         typing = true;
-        socket.emit('typing');
+        socket.emit('typing', {room: activeRoom});
       }
       lastTypingTime = (new Date()).getTime();
 
@@ -162,7 +188,7 @@ $(function() {
         var typingTimer = (new Date()).getTime();
         var timeDiff = typingTimer - lastTypingTime;
         if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-          socket.emit('stop typing');
+          socket.emit('stop typing', {room: activeRoom});
           typing = false;
         }
       }, TYPING_TIMER_LENGTH);
@@ -199,7 +225,7 @@ $(function() {
     if (event.which === 13) {
       if (username) {
         sendMessage();
-        socket.emit('stop typing');
+        socket.emit('stop typing', {room: activeRoom});
         typing = false;
       } else {
         setUsername();
@@ -225,15 +251,16 @@ $(function() {
 
   // Socket events
 
-  // Whenever the server emits 'login', log the login message
+  // Whenever the server emits 'login', log the login message in the global room
+  // to welcome the user to the chat
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
     var message = "Welcome to Socket.IO Chat – ";
-    log(message, {
+    log(message, defaultRoom, {
       prepend: true
     });
-    addParticipantsMessage(data);
+    newUserMessage(data);
   });
 
   // Whenever the server emits 'new message', update the chat body
@@ -241,15 +268,15 @@ $(function() {
     addChatMessage(data);
   });
 
-  // Whenever the server emits 'user joined', log it in the chat body
+  // Whenever the server emits 'user joined', log it in the chat body of the appropriate room
   socket.on('user joined', function (data) {
-    log(data.username + ' joined');
+    log(data.username + ' joined', data.room);
     addParticipantsMessage(data);
   });
 
   // Whenever the server emits 'user left', log it in the chat body
   socket.on('user left', function (data) {
-    log(data.username + ' left');
+    log(data.username + ' left', data.room);
     addParticipantsMessage(data);
     removeChatTyping(data);
   });
@@ -263,4 +290,85 @@ $(function() {
   socket.on('stop typing', function (data) {
     removeChatTyping(data);
   });
+  
+  
+  // Tab stuff
+    var tabTitle = $( "#tab_title" ),
+      tabContent = $( "#tab_content" ),
+      tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
+      tabCounter = 2;
+ 
+    var tabs = $( "#tabs" ).tabs();
+ 
+    // modal dialog init: custom buttons and a "close" callback resetting the form inside
+    var dialog = $( "#dialog" ).dialog({
+      autoOpen: false,
+      modal: true,
+      buttons: {
+        Add: function() {
+          addTab();
+          $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      },
+      close: function() {
+        form[ 0 ].reset();
+      }
+    });
+ 
+    // addTab form: calls addTab function on submit and closes the dialog
+    var form = dialog.find( "form" ).submit(function( event ) {
+      addTab();
+      dialog.dialog( "close" );
+      event.preventDefault();
+    });
+ 
+    // actual addTab function: adds new tab using the input from the form above
+    /*function addTab() {
+      var label = tabTitle.val() || "Tab " + tabCounter,
+        id = "tabs-" + tabCounter,
+        li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
+        tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+ 
+      tabs.find( ".ui-tabs-nav" ).append( li );
+      //tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+      tabs.tabs( "refresh" );
+      tabCounter++;
+    }*/
+    
+    function addTab() {
+      var label = tabTitle.val();
+      var id = label;
+      var li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) );
+      var tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+ 
+      tabs.find( ".ui-tabs-nav" ).append( li );
+      tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+      tabs.tabs( "refresh" );
+      tabCounter++;
+    }
+ 
+    // addTab button: just opens the dialog
+    $( "#add_tab" )
+      .button()
+      .click(function() {
+        dialog.dialog( "open" );
+      });
+ 
+    // close icon: removing the tab on click
+    tabs.delegate( "span.ui-icon-close", "click", function() {
+      var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+      $( "#" + panelId ).remove();
+      tabs.tabs( "refresh" );
+    });
+ 
+    tabs.bind( "keyup", function( event ) {
+      if ( event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE ) {
+        var panelId = tabs.find( ".ui-tabs-active" ).remove().attr( "aria-controls" );
+        $( "#" + panelId ).remove();
+        tabs.tabs( "refresh" );
+      }
+    });
 });
